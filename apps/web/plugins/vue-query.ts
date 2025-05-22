5d315bb7bd5b7290bf0516021409954801088110
Daryll Santos <daryll.santos@gmail.com>
2025-05-22 13:25:22 +0800

    Fix vue-query plugin code

--- apps/web/plugins/vue-query.ts
+++ apps/web/plugins/vue-query.ts
@@ -1,8 +1,22 @@
 import { defineNuxtPlugin } from "#app";
-import { VueQueryPlugin } from "@tanstack/vue-query";
+import {
+  VueQueryPlugin,
+  type VueQueryPluginOptions,
+} from "@tanstack/vue-query";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueQueryPlugin, {
-    enableDevtoolsV6Plugin: true,
-  });
+  const options: VueQueryPluginOptions = {
+    enableDevtoolsV6Plugin: process.env.NODE_ENV !== "production",
+    queryClientConfig: {
+      defaultOptions: {
+        queries: {
+          staleTime: 5 * 60 * 1000, // 5 minutes
+          retry: 1,
+          refetchOnWindowFocus: false,
+        },
+      },
+    },
+  };
+
+  nuxtApp.vueApp.use(VueQueryPlugin, options);
 });
